2ce674ee0fa0b2f651164e796f30a03a9266e022
cfchou <cfchou@gmail.com>
2016-09-26 23:30:01 +0800

    connect no throw

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import asyncio
 import os
 from hbmqtt.client import MQTTClient, ConnectException, ClientException
+from hbmqtt.mqtt import connack
 from hbmqtt.errors import MQTTException
 from hbmqtt.version import get_version
 from docopt import docopt
@@ -18,21 +19,37 @@ import logging
 log = logging.getLogger(__name__)
 
 
+class CgClient(MQTTClient):
+    def __init__(self, client_id=None, config=None, loop=None):
+        super().__init__(client_id, config, loop)
+
 
 async def _do_fire(loop, fire: lc.Fire):
+    async def connect_nothrow(c):
+        try:
+            return await c.connect(uri='mqtt://127.0.0.1:1883')
+        except ConnectException as e:
+            log.exception(e)
+            return connack.SERVER_UNAVAILABLE
+
+    config = {
+        'auto_reconnect': False,
+    }
     log.debug('_do_fire for {} secs'.format(fire.duration))
     history = []
     for i in range(0, fire.duration):
         fire_at = loop.time()
         log.debug('_do_fire for {}/sec'.format(fire.rate))
-        cs = [MQTTClient() for i in range(0, fire.rate)]
-        fire_coros = [c.connect(uri='mqtt://127.0.0.1:1883') for c in cs]
+        cs = [MQTTClient(config=config) for i in range(0, fire.rate)]
+        fire_coros = [connect_nothrow(c) for c in cs]
         fire_coros.append(asyncio.sleep(1))
         coro = asyncio.wait(fire_coros, loop=loop, timeout=3)
-        done, not_done = await coro
-        log.debug('done:{}, not_done:{}'.format(len(done), len(not_done)))
-        history.append(lc.RunnerHistoryItem(at=fire_at, succeeded=len(done)-1,
-                                            failed=len(not_done)))
+        done, _ = await coro
+        succeeded = len([d for d in done
+                         if d.result() != connack.SERVER_UNAVAILABLE]) - 1
+        log.debug('done:{}, succeeded:{}'.format(len(done), succeeded))
+        history.append(lc.RunnerHistoryItem(at=fire_at, succeeded=succeeded,
+                                            failed=fire.rate-succeeded))
     return history
 
 
@@ -77,9 +94,16 @@ def run():
 
 
 async def foo():
-    await asyncio.sleep(5)
-    raise ClientException('WWWWW')
+    try:
+        await bar()
+    except ClientException as e:
+        print(repr(e))
+
     #raise Exception('WWWWW')
+
+async def bar():
+    await asyncio.sleep(2)
+    raise ClientException('WWWWW')
 
 def run2():
     loop = asyncio.get_event_loop()
@@ -91,6 +115,6 @@ if __name__ == '__main__':
     with open('log_config.yaml') as fh:
         try:
             logging.config.dictConfig(yaml.load(fh))
-            run2()
+            run()
         finally:
             logging.shutdown()
