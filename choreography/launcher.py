b3473ddc7357333ce8f556ac805b2bfab37410c0
cfchou <cfchou@gmail.com>
2016-09-28 22:49:48 +0800

    tmp

--- choreography/launcher.py
+++ choreography/launcher.py
@@ -6,10 +6,15 @@ import uuid
 import logging
 log = logging.getLogger(__name__)
 
-Fire = NamedTuple('Fire', [('rate', int), ('duration', int),
+# fire 'rate' clients every 'step' seconds for 'steps_count' times.
+Fire = NamedTuple('Fire', [('rate', int), ('step', int), ('steps_count', int),
                            ('supervisor', str)])
-Idle = NamedTuple('Idle', [])
+
+# don't ever come back to ask
 Terminate = NamedTuple('Terminate', [])
+
+# come back after 'step' * 'steps_count' seconds elapse
+Idle = NamedTuple('Idle', [('step', int), ('steps_count', int)])
 
 
 RunnerHistoryItem = NamedTuple('RunnerHistoryItem',
@@ -51,7 +56,7 @@ class RunnerContext(object):
 
 class Launcher(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def ask_next(self, runner_ctx: RunnerContext) -> LauncherResp:
+    def ask(self, runner_ctx: RunnerContext) -> LauncherResp:
         """
 
         :param runner_ctx:
@@ -60,19 +65,17 @@ class Launcher(metaclass=abc.ABCMeta):
 
 
 class IdleLancher(Launcher):
-    def ask_next(self, runner_ctx: RunnerContext) -> LauncherResp:
+    def ask(self, runner_ctx: RunnerContext) -> LauncherResp:
         log.debug('runner_ctx:{}'.format(runner_ctx))
         return LauncherResp(Idle())
 
 
 class OneShotLancher(Launcher):
-    def ask_next(self, runner_ctx: RunnerContext) -> LauncherResp:
+    def ask(self, runner_ctx: RunnerContext) -> LauncherResp:
         log.debug('runner_ctx:{}'.format(runner_ctx.__dict__))
         if runner_ctx.opaque is not None:
             return LauncherResp(Terminate())
         else:
-            #return LauncherResp(Fire(rate=1, start=0, end=10, supervisor=''),
-            #                    opaque=1)
-            return LauncherResp(Fire(rate=1, duration=1, supervisor=''),
-                                opaque=1)
+            return LauncherResp(Fire(rate=1, step=1, steps_count=1,
+                                     supervisor=''), opaque=1)
 
